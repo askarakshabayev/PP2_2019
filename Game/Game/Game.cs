d6ceaa346a7a56c5bc3bf643bed3625dee9a347c
askarakshabayev <askar.akshabayev@gmail.com>
2019-02-02 10:35:20 +0600

    added game example

---
+++ Game/Game/Game.cs
@@ -0,0 +1,59 @@
+﻿using System;
+namespace Game
+{
+    public class Game
+    {
+        public bool gameOver = false;
+        public int OpenDoorClick1;
+        public int OpenDoorClick2;
+        public int currentDoor;
+
+        public int door1min_amount;
+        public int door1max_amount;
+
+        public Game()
+        {
+            OpenDoorClick1 = 0;
+            OpenDoorClick2 = 0;
+            currentDoor = 0;
+
+            door1min_amount = 1;
+            door1max_amount = 10;
+
+
+        }
+        public void ShowMenu()
+        {
+            Console.Clear();
+            if (currentDoor == 1)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("1. Red door opened");
+            } else {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("1. Red door");
+            }
+
+            if (currentDoor == 2)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("2. Blue door opened");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("2. Blue door");
+            }
+            if (currentDoor == 3)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("3. Green door opened");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("3. Green door");
+            }
+        }
+    }
+}
