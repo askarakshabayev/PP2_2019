463b4ac0eae8ce95a239578f165755f5e82d8945
askarakshabayev <askar.akshabayev@gmail.com>
2019-02-02 09:28:14 +0600

    added examples from week2 group 2

---
+++ week2/G2/ExStatic/ExStatic/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ExStatic
+{
+    class Test
+    {
+        public static int a;
+        public int b;
+
+        public void incA()
+        {
+            a++;
+        }
+        public void incB()
+        {
+            b++;
+        }
+
+        public override string ToString()
+        {
+            return a + " " + b;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test t1 = new Test();
+            Test t2 = new Test();
+            t1.incA();
+            t2.incA();
+            t1.incB();
+            t2.incB();
+            Test.a++;
+            Console.WriteLine(t1);
+            Console.WriteLine(t2);
+        }
+    }
+}
---
+++ week2/G2/Example2/Example2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Example2
+{
+    class Complex
+    {
+        int a, b;
+
+        public Complex(int a_, int b_)
+        {
+            a = a_;
+            b = b_;
+        }
+        public Complex() { }
+
+        public void Read()
+        {
+            String[] s = Console.ReadLine().Split("/"); // 1/2
+            a = int.Parse(s[0]);
+            b = int.Parse(s[1]);
+        }
+
+        public override string ToString()
+        {
+            return a + "/" + b;
+        }
+
+        public static Complex operator +(Complex c1, Complex c2)
+        {
+            Complex c3 = new Complex();
+            c3.b = c1.b * c2.b;
+            c3.a = c1.a * c2.b + c1.b * c2.a;
+            return c3;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Complex c1 = new Complex();
+            Complex c2 = new Complex();
+            c1.Read();
+            c2.Read();
+            Complex c3 = c1 + c2;
+            Console.WriteLine(c1);
+            Console.WriteLine(c2);
+            Console.WriteLine("c1 + c2 = " + c3);
+        }
+    }
+}
