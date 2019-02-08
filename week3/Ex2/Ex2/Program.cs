f7cb14edb52c938ab52e624bace69528f564ab2d
askarakshabayev <askar.akshabayev@gmail.com>
2019-02-08 19:06:30 +0600

    added week3 examples

---
+++ week3/Ex2/Ex2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int x = 0;
+                int y = 0;
+                while (true)
+                {
+                    try
+                    {
+                        ConsoleKeyInfo consoleKey = Console.ReadKey();
+                    }catch(Exception e)
+                    {
+
+                    }
+                    if (consoleKey.Key == ConsoleKey.UpArrow)
+                        y--;
+                    if (consoleKey.Key == ConsoleKey.DownArrow)
+                        y++;
+                    if (consoleKey.Key == ConsoleKey.RightArrow)
+                        x++;
+                    if (consoleKey.Key == ConsoleKey.LeftArrow)
+                        x--;
+                    Console.Clear();
+                    Console.SetCursorPosition(x, y);
+                    Console.WriteLine("*");
+                }
+            }catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
