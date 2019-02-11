0ebd56cd270e7e0bc89adf9f324e740c45ad332e
askarakshabayev <askar.akshabayev@gmail.com>
2019-02-11 15:53:22 +0600

    added group 2 examples

---
+++ week3/G2/Ex2/Ex2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetCursorPosition(10, 10);
+            Console.WriteLine("Hello world");
+        }
+    }
+}
