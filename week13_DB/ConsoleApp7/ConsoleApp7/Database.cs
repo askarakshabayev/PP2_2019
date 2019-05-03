4bd052d16226bb0c00d31de320ab129931caa134
askarakshabayev <askar.akshabayev@gmail.com>
2019-05-03 02:17:13 -0700

    added db example

---
+++ week13_DB/ConsoleApp7/ConsoleApp7/Database.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.IO;
+
+namespace ConsoleApp7
+{
+    class Database
+    {
+        public SQLiteConnection connection;
+
+        public Database()
+        {
+            string dbName = @"C:\Users\Askar\Desktop\intranet.db";
+            connection = new SQLiteConnection(@"Data Source=" + dbName);
+            if (!File.Exists(dbName))
+            {
+                Console.WriteLine("Database created");
+                SQLiteConnection.CreateFile(dbName);
+            }
+        }
+
+        public void OpenConnection()
+        {
+            if (connection.State != System.Data.ConnectionState.Open)
+                connection.Open();
+        }
+
+        public void CloseConnection()
+        {
+            if (connection.State != System.Data.ConnectionState.Closed)
+                connection.Close();
+        }
+    }
+}
---
+++ week13_DB/ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /**
+             * INSERT EXAMPLE (DELETE, UPDATE)
+             
+            Database database = new Database();
+            string query = "insert into students(name, surname) values(@name, @surname)";
+            database.OpenConnection();
+            SQLiteCommand command = new SQLiteCommand(query, database.connection);
+            command.Parameters.AddWithValue("@name", "name3");
+            command.Parameters.AddWithValue("@surname", "surname3");
+            command.ExecuteNonQuery();
+            database.CloseConnection();
+            Console.ReadKey();
+            */
+            /**
+             * SELECT EXAMPLE
+             */
+            Database database = new Database();
+            string query = "select * from students where id > 1";
+            database.OpenConnection();
+            SQLiteCommand command = new SQLiteCommand(query, database.connection);
+            SQLiteDataReader result = command.ExecuteReader();
+
+            if (result.HasRows)
+            {
+                while (result.Read())
+                {
+                    Console.WriteLine("id: {2}, name: {0}, surname: {1}", result["name"], result["surname"], result["id"]);
+                }
+            }
+            database.CloseConnection();
+            Console.ReadKey();
+
+        }
+    }
+}
